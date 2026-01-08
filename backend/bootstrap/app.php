edbc34096e5c4b531fe2e17fe777c7a8a39bebcf
ahr-ahr <mp2pq3b8@outlook.com>
2026-01-08 20:04:20 +0700

    feat(auth): setup Laravel Sanctum SPA auth & secure CORS.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,16 +3,35 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\HandleCors;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        /**
+         * =================================================
+         * API Middleware (Laravel 12 + Sanctum SPA)
+         * =================================================
+         */
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        /**
+         * =================================================
+         * Global Middleware
+         * =================================================
+         */
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
